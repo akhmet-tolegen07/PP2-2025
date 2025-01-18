e1ea461ef12687e41c5061a5e1ca92af6bc41250
akhmet-tolegen07 <ak_tolegen@kbtu.kz>
2025-01-18 20:24:14 +0500

    first commit

---
+++ lab1/3datatypes.py
@@ -0,0 +1,27 @@
+integer = 5
+float = 5.5
+letter = 'A'
+word = "Doner"
+comlex = 1j
+list = ["apple", "banana"]
+tuple = ("apple", "banana")
+set = {"apple", "banana"} # or set()
+dict = {"fruits" : "apple"}
+bool = True
+range = range(6)
+frozenset = frozenset({"apple", "banana"})
+byte = b"apple"
+
+print(type(integer))
+print(type(float))
+print(type(letter))
+print(type(word))
+print(type(comlex))
+print(type(list))
+print(type(tuple))
+print(type(set))
+print(type(dict))
+print(type(bool))
+print(type(range))
+print(type(frozenset))
+print(type(byte))
